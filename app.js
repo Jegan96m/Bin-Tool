7cf5c8c832ed900d655b43e9bd1c63d12d612297
Jegan96m <jeganmoorali.JM@gmail.com>
2025-03-18 11:53:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ var map = L.map('map', {
         }).addTo(map);
 
 var darkLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '<span style="font-size: 16px;>© Develop by AzbinFahmi</span>',
+    attribution: '© Develop by AzbinFahmi',
     maxZoom: 99
 });
 var lightLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '<span style="font-size: 16px;>© Develop by AzbinFahmi</span>',
+    attribution: '© Develop by AzbinFahmi',
     maxZoom: 99
 });
 // Add the light layer by default
